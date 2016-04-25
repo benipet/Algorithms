92fe61c5faef60caebda5201e1dae91fbea50a8c
Peter <peter@debian-test.debtest>
2016-04-25 17:19:54 +0100

    Blocks committed

--- BlockProblem/src/block.c
+++ BlockProblem/src/block.c
@@ -1,19 +1,19 @@
 #include "block.h"
 
 struct _position{
-	unsigned int	index;
-	pos*		addr_table;
-	pos*		addr_next;
-	blck*		addr_block;
+	int		index;		//Position index
+	pos*		addr_table;	//Address of table
+	pos*		addr_next;	//Address of next position
+	blck*		addr_block;	//Address of the block
 
 };
 
 struct _block{
-	unsigned int	index;
-	pos*		base;
-	blck*		above;
-	blck*		bottom;
-	pos*		pos;
+	int		index;		//index of block
+	pos*		base;		//addr of base
+	blck*		above;		//addr of above block
+	blck*		bottom;		//addr of bottom block
+	pos*		pos;		//addr of position
 };
 
 
@@ -31,9 +31,9 @@ pos* init_table(void){
 	pos* table = (pos*)malloc(sizeof(pos));
 	table->index=0;
 	table->addr_table=table;
-	blck* myChild = init_block(table);
-	myChild->bottom=myChild;
-
+	table->addr_next=0;
+	table->addr_block=init_block(table);c
+	
 	return table;
 	}
 
@@ -46,8 +46,7 @@ pos* build_positions(pos* table, int size){
 	new->index=counter;
 	new->addr_table=old->addr_table;
 	//call the init_block function
-	blck* myChild = init_block(new);
-	myChild->bottom=myChild;
+	new->addr_block=init_block(new);
 	old=new;
 	}
 
@@ -55,29 +54,32 @@ pos* build_positions(pos* table, int size){
 
 blck* init_block(pos* base){
 	blck* new_block = (blck*)malloc(sizeof(blck));
-	new_block->index = base->index;
+	new_block->index=base->index;
 	new_block->base=base;
 	new_block->above=0;
-	/*bottom link ?*/
+	new_block->bottom=0;
 	new_block->pos=base;
 	return new_block;
 }
+
 int* link_block(blck* linker, blck* receiver){
 	if(receiver->above!=0){
 	printf("Sorry I have above a block\n");
 	}
 	else{
-	receiver->above=linker;	
 	linker->bottom=receiver;
 	linker->pos=receiver->pos;
+	receiver->above=linker;
 	}
 	
 }
    /*Receiver: the bottom blolck*/
-blck* ulink_block(blck* b_block,blck* a_block){
-	b_block->above=0;
-	a_block->bottom=0;
-
+blck* ulink_block(blck* leaver){
+	if(leaver->bottom!=0){
+	leaver->bottom->above=0;
+	}
+	leaver->bottom=0;
+	leaver->pos=leaver->base;
 }	/*bottom block and aboce block*/
 pos* get_blpos(blck* block);			/*Returns the position addr. block*/
 pos* get_base(blck* block);			/*Returns the base position addr.*/
--- BlockProblem/src/block.h
+++ BlockProblem/src/block.h
@@ -19,7 +19,7 @@ pos* build_positions(pos* table, int size);
 
 blck* init_block(pos* base);
 int* link_block(blck* linker, blck* receiver);   /*Receiver: the bottom blolck*/
-blck* ulink_block(blck* b_block,blck* a_block);	/*bottom block and aboce block*/
+blck* ulink_block(blck* leaver);		/*bottom block and aboce block*/
 pos* get_blpos(blck* block);			/*Returns the position addr. block*/
 pos* get_base(blck* block);			/*Returns the base position addr.*/
 
